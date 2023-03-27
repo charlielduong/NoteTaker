887903da4a74f3ee74f52a2af493af99a021b8c2
charlielduong <charlielduong@gmail.com>
2023-03-27 17:55:50 -0400

    Routing, Vuex, Components/Views/Routes, and Token Authentication

--- fastapi-vue/services/frontend/src/main.js
+++ fastapi-vue/services/frontend/src/main.js
@@ -4,11 +4,25 @@ import axios from 'axios';
 
 import App from './App.vue';
 import router from './router';
+import store from './store';
 
 const app = createApp(App);
 
 axios.defaults.withCredentials = true;
 axios.defaults.baseURL = 'http://localhost:5001/';  // the FastAPI backend
 
+// NEW
+axios.interceptors.response.use(undefined, function (error) {
+  if (error) {
+    const originalRequest = error.config;
+    if (error.response.status === 401 && !originalRequest._retry) {
+      originalRequest._retry = true;
+      store.dispatch('logOut');
+      return router.push('/login')
+    }
+  }
+});
+
 app.use(router);
+app.use(store);
 app.mount("#app");
